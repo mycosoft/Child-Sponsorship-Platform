d3d54dbafdfab71c97b95e7f596235a9ef29720f
Myco S. Kagame <34786713+mycosoft@users.noreply.github.com>
2023-12-05 09:58:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function sponsorChild(childId) {
         data: { childId: childId },
         dataType: 'json',
         success: function (response) {
-            // Display a confirmation message (replace with your own UI logic)
+            
             alert(response.message);
         },
         error: function (error) {
